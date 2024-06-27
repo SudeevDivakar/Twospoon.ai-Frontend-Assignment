2beb612ddff43dc6e024874eddef31a03003b67d
Sudeev Divakar <sudeev.divakar@gmail.com>
2024-06-27 18:57:00 +0530

    Finish Webpage for Desktops

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -5,9 +5,54 @@ export default function MainContent() {
   return (
     <section className="w-full flex justify-center">
       <div className="width-80 h-full mt-8 flex justify-between">
-        <section className="perfect-width perfect-height bg-zinc-900"></section>
-        <section className="perfect-width perfect-height">
-          <h1 className="font-semibold text-2xl mb-5">Payment</h1>
+        <section className="perfect-width border-2 rounded-lg flex flex-col">
+          <section className="width-90 flex flex-col self-center items-center">
+            <h1 className="w-full text-2xl font-semibold mt-7 mb-3">Summary</h1>
+            <div className="bg-gray-100 w-full flex my-2 rounded-xl justify-around py-3 pl-3 pr-5 items-center">
+              <img className="w-12" src="./iphone.jpg" alt="" />
+              <h2 className="grow ml-5">Apple iPhone 14 Pro Max 128Gb</h2>
+              <h2 className="font-bold">$1399</h2>
+            </div>
+            <div className="bg-gray-100 w-full flex my-2 rounded-xl justify-around py-3 pl-3 pr-5 items-center">
+              <img
+                className="w-12 rounded-full"
+                src="./headphones.jpg"
+                alt=""
+              />
+              <h2 className="grow ml-5">AirPods Max Silver</h2>
+              <h2 className="font-bold">$549</h2>
+            </div>
+            <div className="bg-gray-100 w-full flex my-2 rounded-xl justify-around py-3 pl-3 pr-5 items-center">
+              <img className="w-12 rounded-full" src="./watch.jpg" alt="" />
+              <h2 className="grow ml-5">Apple Watch Series 9 GPS 41mm</h2>
+              <h2 className="font-bold">$399</h2>
+            </div>
+            <h2 className="w-full mt-2 mb-2 text-gray-600">Address</h2>
+            <h3 className="w-full mb-3">
+              1131 Dusty Townline, Jacksonville, TX 40322
+            </h3>
+            <h2 className="w-full mt-2 mb-2 text-gray-600">Shipment method</h2>
+            <h3 className="w-full">Free</h3>
+            <div className="w-full flex justify-between mt-6">
+              <h2 className="font-semibold">Subtotal</h2>
+              <h2 className="font-semibold">$2347</h2>
+            </div>
+            <div className="w-full flex justify-between mt-6">
+              <h3 className="text-gray-500">Estimated Tax</h3>
+              <h3 className="font-semibold">$50</h3>
+            </div>
+            <div className="w-full flex justify-between mt-5">
+              <h3 className="text-gray-500">Estimated shipping & Handling</h3>
+              <h3 className="font-semibold">$29</h3>
+            </div>
+            <div className="w-full flex justify-between mt-6">
+              <h2 className="font-semibold">Total</h2>
+              <h2 className="font-bold">$2426</h2>
+            </div>
+          </section>
+        </section>
+        <section className="perfect-width">
+          <h1 className="font-bold text-2xl mb-5">Payment</h1>
           <a className="mr-10 py-1 border-b-2 border-b-black" href="#">
             <span>Credit Card</span>
           </a>
@@ -52,7 +97,7 @@ export default function MainContent() {
             </label>
           </div>
           <div className="flex justify-between">
-            <button className="py-6 bg-white border border-black text-black rounded-lg hover:bg-gray-400 hover:text-white hover:border-0 perfect-width-form bg-transition">
+            <button className="py-6 bg-white border border-black text-black rounded-lg hover:bg-gray-400 hover:text-white perfect-width-form bg-transition">
               Back
             </button>
             <button className="px-6 py-3 bg-black text-white rounded-lg hover:bg-gray-500 perfect-width-form bg-transition">
